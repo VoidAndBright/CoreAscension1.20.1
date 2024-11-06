2fa4bf5b46961e13430381e90ce282f17242c118
Void <voidandvoid@hotmail.com>
2024-11-06 21:22:49 +0000

    SurfaceRules

--- src/main/java/com/blah/coreascension/world/CoreAscensionWorldGeneration.java
+++ src/main/java/com/blah/coreascension/world/CoreAscensionWorldGeneration.java
@@ -1,10 +1,12 @@
 package com.blah.coreascension.world;
 
 import com.blah.coreascension.world.biome.CoreAscensionBiomeGeneration;
+import com.blah.coreascension.world.biome.CoreAscensionSurfaceRules;
 
 public class CoreAscensionWorldGeneration {
     public static void GenerateWorldGeneration(){
         CoreAscensionBiomeGeneration.GenerateBiomes();
+        CoreAscensionSurfaceRules.GenerateSurfaceRules();
     }
     // don't remove anything here as it for future features such as ore, trees and geodes
 }
--- src/main/java/com/blah/coreascension/world/biome/CoreAscensionSurfaceRules.java
+++ src/main/java/com/blah/coreascension/world/biome/CoreAscensionSurfaceRules.java
@@ -1,39 +1,27 @@
 package com.blah.coreascension.world.biome;
 
 import com.terraformersmc.biolith.api.surface.SurfaceGeneration;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.VerticalSurfaceType;
 import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.gen.noise.NoiseParametersKeys;
 import net.minecraft.world.gen.surfacebuilder.MaterialRules;
 
-import static net.minecraft.world.gen.surfacebuilder.MaterialRules.*;
 
 public class CoreAscensionSurfaceRules {
-    static MaterialRules.MaterialRule birchForest = condition(MaterialRules.biome(BiomeKeys.BIRCH_FOREST),
-            block(Blocks.CALCITE.getDefaultState()));
-
-    static MaterialRules.MaterialRule crimsonForest = condition(MaterialRules.biome(BiomeKeys.CRIMSON_FOREST), sequence(
-            condition(STONE_DEPTH_FLOOR,
-                    MaterialRules.sequence(
-                            condition(
-                                    MaterialRules.noiseThreshold(NoiseParametersKeys.NETHER_WART, 1.17),
-                                    block(Blocks.NETHER_WART_BLOCK.getDefaultState())),
-                            block(Blocks.CRIMSON_NYLIUM.getDefaultState()))),
-            block(Blocks.NETHERRACK.getDefaultState())));
-
-    static MaterialRules.MaterialRule warpedForest = condition(MaterialRules.biome(BiomeKeys.WARPED_FOREST), sequence(
-            condition(STONE_DEPTH_FLOOR,
-                    MaterialRules.sequence(
-                            condition(
-                                    MaterialRules.noiseThreshold(NoiseParametersKeys.NETHER_WART, 1.17),
-                                    block(Blocks.WARPED_WART_BLOCK.getDefaultState())),
-                            block(Blocks.WARPED_NYLIUM.getDefaultState()))),
-            block(Blocks.NETHERRACK.getDefaultState())));
-
+    public static MaterialRules.MaterialRule TROPICS_SURFACE =
+        MaterialRules.condition(MaterialRules.biome(BiomeKeys.PLAINS),
+            MaterialRules.sequence(
+                MaterialRules.condition(MaterialRules.stoneDepth(2, true, VerticalSurfaceType.FLOOR), block(Blocks.SAND)),
+                block(Blocks.SANDSTONE)
+            )
+        );
+    private static MaterialRules.MaterialRule block(Block block) {
+        return MaterialRules.block(block.getDefaultState());
+    }
     public static void GenerateSurfaceRules(){
-        SurfaceGeneration.addOverworldSurfaceRules(
-                Identifier.of("minecraft", "rules/overworld"),
-                condition(surface(), sequence(birchForest, crimsonForest, warpedForest)));
+        SurfaceGeneration.addOverworldSurfaceRules(new Identifier("minecraft","rules/overworld"),
+                MaterialRules.condition(MaterialRules.surface(), MaterialRules.sequence(TROPICS_SURFACE)));
     }
 }
